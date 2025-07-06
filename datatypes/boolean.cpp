579e3da6d29dfb8db67ef321d5876cee831f40aa
stevehuang <xhtxsteve@163.com>
2025-07-06 18:59:32 +0800

    20250706update

---
+++ datatypes/boolean.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    bool a = true;
+    bool b = false;
+
+    cout << a << '\n'; //1表示true
+    cout << b << '\n'; //0表示false
+    a = !a;
+    cout << a << '\n';
+    cout << sizeof(a) << '\n'; //boolean占用一个字节
+    // while (a && !b)
+    // {
+    //     cout << 1 << endl;
+    // };
+
+    return 0;
+}
---
+++ input.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 5;
+    cin >> a;
+    cout << "The current value of a is "<< a << '\n';
+}
